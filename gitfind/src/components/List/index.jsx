d1630bd731d377c36f3978977804f19cb8976499
Jh0ny1 <mendesserafimjoaopedro@email.com>
2024-09-17 14:15:08 -0300

    projeto completo
    Mudar Read.me

---
+++ gitfind/src/components/List/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './styles.css'; 
+
+function ItemList({title, description}){
+return <div className="item-list"> 
+    <strong>{title}</strong>
+    <p>{description}</p>
+    <hr></hr>
+    </div>
+
+}
+export default ItemList;
